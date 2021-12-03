f65221123e613bdf962f3af43c6b5f2d01e5bfd8
Anigdha <69959680+Anigdha@users.noreply.github.com>
2021-12-03 07:46:48 +0530

    translation functionality added

--- banana.js
+++ banana.js
@@ -1,10 +1,12 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var textInput=document.querySelector("#text-input");
+var outputDiv=document.querySelector("#output");
 //console.log(textInput);
 function clickHandler()
 {
-    console.log("clicked!");
-    console.log("input",textInput.value);
+    // console.log("clicked!");
+    // console.log("input",textInput.value);
+    outputDiv.innerText="ajahayabjbau"+textInput.value;
 }
 btnTranslate.addEventListener("click",clickHandler);
 
